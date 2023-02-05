6f80639c8c612abea1bcc14cd278ab1de68cc76b
Ner Karso <nerpkarso@gmail.com>
2023-02-05 18:55:59 +0000

    Add database SSL option

--- server/database.js
+++ server/database.js
@@ -5,15 +5,19 @@ const { createConnection } = require('mysql2/promise');
  */
 exports.createConnection = async function (req, res, next) {
   res.db = null;
+  let connectionOptions = {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  };
+  if (process.env.DB_SSL === 'true') {
+    connectionOptions['ssl'] = {};
+  }
   try {
     // Pass the connection
-    res.db = await createConnection({
-      host: process.env.DB_HOST,
-      port: 3306,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
+    res.db = await createConnection(connectionOptions);
     console.log('[Database] Connection established');
   } catch (error) {
     console.error('[Database] Error: ', error.message);
